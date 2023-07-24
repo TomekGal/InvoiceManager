4ac641f81be70bf8a9bb10aedc7a3121cab2e9a1
TomekGal <75546362+TomekGal@users.noreply.github.com>
2023-07-24 13:33:27 +0200

    ManageClients

--- InvoiceManager/Models/Repositories/ClientRepository.cs
+++ InvoiceManager/Models/Repositories/ClientRepository.cs
@@ -1,4 +1,5 @@
 ﻿using InvoiceManager.Models.Domains;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,12 +8,56 @@ namespace InvoiceManager.Models.Repositories
 {
     public class ClientRepository
     {
-
+       
         public List<Client> GetClients(string userId)
         {
            using(var context = new ApplicationDbContext())
             {
                 return context.Clients.Where(x => x.UserId == userId).ToList();
+            }
+        }
+
+        public Address GetAdress(int addressId)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+                return context.Addresses.SingleOrDefault(x => x.Id == addressId);
+            }
+           
+        }
+
+        public void Add(Client client)
+        {
+            using (var context = new ApplicationDbContext())
+            {
+               
+                context.Clients.Add(client);
+                context.SaveChanges();
+            }
+        }
+
+        public Client GetClient(int id)
+        {
+
+            using (var context = new ApplicationDbContext())
+            {
+               return context.Clients.SingleOrDefault(x => x.Id == id);
+
+            }
+        }
+
+        public void Update(Client client)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+           using(var context = new ApplicationDbContext())
+            {
+              var clientToDelete=  context.Clients.SingleOrDefault(x => x.Id == id);
+                context.Clients.Remove(clientToDelete);
+                context.SaveChanges();
             }
         }
     }
